c43b68a5411e9625c1ccd36c5a2f97818ba4575a
cphsiung <chunpeihs@gmail.com>
2021-07-25 15:31:08 -0700

    Add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+import json
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please check your input."
+
+word = input("Enter a word: ")
+
+print(translate(word))
